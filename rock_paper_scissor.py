fe12edd4796b5dc1c7512adc9ef10dc9be1a9381
Research Devkota <134274596+itsresearch@users.noreply.github.com>
2026-01-07 19:45:07 +0545

    Add files via upload

---
+++ rock_paper_scissor.py
@@ -0,0 +1,45 @@
+# rock paper scissor game.
+
+import random
+
+print("========= Rock Paper Scissors Game ==========")
+
+choices = ["rock", "paper", "scissors"]
+while True:
+    user = input("\nEnter rock, paper, or scissors: ").lower()
+    if user not in choices:
+        print("Invalid choice. Please try again.")
+        continue
+    computer = random.choice(choices)
+    print("Computer chose: ", computer)
+
+    # logic of the game.
+    if user == computer:
+        print("Its a tie.")
+    elif (
+        (user == "rock" and computer == "scissors")
+        or (user == "paper" and computer == "rock")
+        or (user == "scissors" and computer == "paper")
+    ):
+        print("You win.")
+    else:
+        print("You loose.")
+
+    # play again.
+    again = input("Play again? (yes/no): ").lower()
+    if again != "yes":
+        print("Thanks for playing.")
+        break
+
+
+# AND = Both/All condition True =====> True. else false.
+# True and False ======> False.
+# True and True=======> True
+# False and False=====> False
+# True and(False and True)========> False
+
+# OR = Any condition True======> True.
+# True or False =====> True
+# False or True ======> True
+# False or (true or false) =====> True
+# false and (true or False)=====> false
